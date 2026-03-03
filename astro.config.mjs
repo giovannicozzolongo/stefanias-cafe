5a672ae3b6ff4aeae242ef91e8afc94238329f74
Giovanni Cozzolongo <giovannicozzolongo@live.com>
2026-03-03 09:12:48 +0100

    fix: aggiungo site e base path per GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://giovannicozzolongo.github.io',
+  base: '/stefanias-cafe',
   vite: {
     plugins: [tailwindcss()]
   }
